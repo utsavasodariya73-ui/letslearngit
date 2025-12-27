704a1eed1b716664331fd3fd30a969bd18acd797
utsavasodariya73-ui <utsavasodariya73@gmail.com>
2025-12-27 13:54:16 +0530

    add implement

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 class Addition{
     public static void main(String[] args){
-        int a = 10;
-        int b = 20;
+        int a = 40;
+        int b = 50;
         int sum = a+b;
         System.out.println("Sum =" + sum);
     }
